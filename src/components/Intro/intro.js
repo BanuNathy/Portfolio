1ec1724c5dda72f9771ab347af947e385b4f948b
BanuNathy <banu080419@gmail.com>
2024-06-18 01:46:31 +0530

    Add intro

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -9,7 +9,7 @@ const Intro = () => {
         <img src={bg} alt="Profile" className="bg" />
         <div className="introContent">
             <span className="hello">Hello,</span>
-            <span className="introText">I'm<span className="introName">Banu</span><br />Web Developer</span>
+            <span className="introText">I'm<span className="introName"> Banu</span><br />Web Developer</span>
             <p className="introPara">I'm a skilled web designer with experience in creating <br />visually appesling and user friendly websites.</p>
             <Link><button className="btn">Hire Me</button></Link>
         </div>  
