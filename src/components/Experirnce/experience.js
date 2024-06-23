9107d414ee8a8a8b9cf69f51eaff3e332cdc25a7
BanuNathy <banu080419@gmail.com>
2024-06-23 06:42:21 +0530

    added new component

---
+++ src/components/Experirnce/experience.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './experience.css';
+import skills from '../../data/skills.json';
+
+const Experience = () => {
+  return (
+    <section id="experience">
+        <h2>Experience</h2>
+        <div>
+            <div>
+                <ul></ul>
+            </div>
+        </div>
+    </section>
+  );
+};
+
+export default Experience;
